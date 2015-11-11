6a2eb0a0dc9f3ec42424c8c057ca19622f1bcae1
David Hill <david@davidhill.ie>
2015-11-11 00:38:45 +0000

    Add autoprefixer to sass task

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -4,6 +4,7 @@ module.exports = function (gulp, plugins) {
             .pipe(plugins.sourcemaps.init())
             //.pipe(debug({title: 'unicorn:'}))
             .pipe(plugins.sass().on('error', plugins.sass.logError))
+            .pipe(plugins.autoprefixer({ browsers: ['> 1%', 'IE 9'], cascade: false }))
             .pipe(plugins.sourcemaps.write('./maps'))
             .pipe(gulp.dest('dest/css'));
 
