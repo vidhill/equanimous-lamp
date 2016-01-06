083fcd54fd93150597ac4384d3eba4b693b6a432
David Hill <david@davidhill.ie>
2016-01-06 00:19:00 +0000

    Add Dynamic Links to nav

--- src/app.js
+++ src/app.js
@@ -3,8 +3,17 @@ angular.module('dhApp', [
     'homePage'
     ])
     .config(['$routeProvider', function($routeProvider){
-        $routeProvider.
-            otherwise({
+        $routeProvider
+            .otherwise({
                 redirectTo: '/'
             });
-    }]);;
+
+    }])
+    .controller('dhNavCtrl', ['$scope', function($scope){
+        $scope.links = [
+            { title: 'Link 1', path: 'link1' }, 
+            { title: 'Link 2', path: 'link2' }, 
+            { title: 'Link 3', path: 'link3' }, 
+            { title: 'Link 4', path: 'link4' }
+        ];
+    }]);
