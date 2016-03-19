bd82ab47187a924151ac99a090b61c81acd60931
David Hill <david@davidhill.ie>
2016-03-19 01:55:09 +0000

    Adding more eslint rules

--- angular-modules/pages/home/home.js
+++ angular-modules/pages/home/home.js
@@ -1,5 +1,5 @@
 angular.module('homePage', ['sampleDirective'])
-    .config(['$routeProvider', function($routeProvider){
+    .config(['$routeProvider', function ($routeProvider){
         $routeProvider.
             when('/', {
                 templateUrl: 'angular-modules/pages/home/home.html'
